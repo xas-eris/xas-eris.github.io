08f451b62ffd165cafa6a9396e5029ea711bf962
David Cusick <djcusick@uw.edu>
2019-05-08 16:53:00 -0700

    test csv download

--- js/convertToCSV.js
+++ js/convertToCSV.js
@@ -28,3 +28,23 @@ function convertArrayOfObjectsToCSV(args) {
 
         return result;
     }
+
+function downloadCSV(args) {  
+        var data, filename, link;
+        var csv = convertArrayOfObjectsToCSV({
+            data: mydata
+        });
+        if (csv == null) return;
+
+        filename = args.filename || 'export.csv';
+
+        if (!csv.match(/^data:text\/csv/i)) {
+            csv = 'data:text/csv;charset=utf-8,' + csv;
+        }
+        data = encodeURI(csv);
+
+        link = document.createElement('a');
+        link.setAttribute('href', data);
+        link.setAttribute('download', filename);
+        link.click();
+    }
